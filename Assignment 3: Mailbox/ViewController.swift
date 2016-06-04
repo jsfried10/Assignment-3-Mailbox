54775e20f0c1eb659a965ccbeff0ebfb283196b3
Jeremy Friedland <jeremyfriedland@gmail.com>
2016-06-04 08:21:11 -0700

    Initial Commit

---
+++ Assignment 3: Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment 3: Mailbox
+//
+//  Created by Jeremy Friedland on 6/4/16.
+//  Copyright © 2016 Jeremy Friedland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
